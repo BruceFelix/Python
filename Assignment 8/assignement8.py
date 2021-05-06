4b4c726cb98f8bc4a297d6b93fb74868155e789a
Bruce <44478872+BruceFelix@users.noreply.github.com>
2021-05-06 22:11:10 +0300

    Add files via upload

---
+++ Assignment 8/assignement8.py
@@ -0,0 +1,17 @@
+# The JSON gitHub url link is for English to lolcat translation
+# This program is desingned and already working. Our job is to add Sheng words and meanings in English
+# To the same dictionary
+# Save the JSON file in local TXT file and make it appendable/you should be able to add new terms.
+import json, requests
+data = 'https://raw.githubusercontent.com/normansimonr/Dumb-Cogs/master/lolz/tranzlashun.json'
+resp = requests.get(data)
+dictionary = json.loads(resp.text)
+
+#print(dictionary)
+
+word = input('Original words: ')
+
+if word in dictionary:
+    print('The translation is:', dictionary[word] )
+else:
+    print('Sorry, that word has no translation in the dictionary')
