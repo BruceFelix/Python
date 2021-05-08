8067d53f6afa739091d4e28fa2d5e4451fc255ea
BruceFelix <brucefelixm@gmail.com>
2021-05-08 14:12:35 +0300

    Initialized Assignment9
    No code was added

---
+++ Assignment9/assignment9.py
@@ -0,0 +1,7 @@
+"""
+You finally secured a permanent job with Kenya Power and Lighting Company (KPLC) as a Python and Database programmer.
+As a company, KPLC recently experienced exponential growth due to the Rural Electrification in Kenya. Alot of homes in Kenya,
+both in Urban and Rural areas got the KPLC services. It has been hectic with the old billing system and the consumption 
+and billing has been accurate. As a new Programmer, there is lots of hope in the design of the system you are working on.
+The management and the higher echelons in the company cannot wait long to see the product.
+"""
