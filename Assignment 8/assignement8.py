098505c7cb1a187e9f1fbaea0ea9b681662980e7
BruceFelix <brucefelixm@gmail.com>
2021-05-09 20:15:46 +0300

    added text files
    the program can now append to external files from the dictionaries made

--- Assignment 8/assignement8.py
+++ Assignment 8/assignement8.py
@@ -2,17 +2,51 @@
 # This program is desingned and already working. Our job is to add Sheng words and meanings in English
 # To the same dictionary
 # Save the JSON file in local TXT file and make it appendable/you should be able to add new terms.
-import json, requests
+########################################################################
+#functions
+def dictionary_key_value():
+    with open("dictionary.txt",'a+') as file:#This stores the key:pair value of the sheng word and it's meaning
+        file.write(str(dictionary))
+def dictionary_key():
+    with open("dictionary-key.txt", "a+") as file:# This file stores the key from the dictionary
+        file.write(str(dictionary.keys()))
+def dictionary_value():
+    with open("dictionary-value.txt","a+") as file:# This file stores the values from the dictionary
+        file.write(str(dictionary.values()))
+########################################################################
+"""import json, requests
 data = 'https://raw.githubusercontent.com/normansimonr/Dumb-Cogs/master/lolz/tranzlashun.json'
 resp = requests.get(data)
-dictionary = json.loads(resp.text)
+print(resp)
+dictionary = eval(resp.text)
+#There seems to be a problem with the json fil
+print(dictionary)"""
 
-#print(dictionary)
 
-word = input('Original words: ')
+dictionary = {}
+while True:
+    print("Do you want to add a new sheng word or get it's translation")
+    print("Yes to continue: No to cancel")
+    answer = input().upper()
+    if answer == "YES":
+            word = input('Original words: ')
+            if word in dictionary:
+                print('The translation is:', dictionary[word] )
+            elif word not in dictionary:
+                dictionary[word] =input("Enter it's meaning in English: ")
+            else:
+                
+                continue
+    elif answer == "NO":
+        print("Okay good luck")
+        break
+    else:
+        print("Please choose yes or no")
+print(dictionary)
+print("This dictionary will be stored in a file")
+dictionary_key_value()
+dictionary_key()
+dictionary_value()
 
-if word in dictionary:
-    print('The translation is:', dictionary[word] )
-else:
-    print('Sorry, that word has no translation in the dictionary')
-#There seems to be a problem with the json file
+
+
