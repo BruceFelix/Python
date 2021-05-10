c5cb681d75b01bf9f1b97b20701b7eff16b48b8d
BruceFelix <brucefelixm@gmail.com>
2021-05-10 12:24:12 +0300

    Working
    The program can now make request and append new values

--- Assignment 8/assignement8.py
+++ Assignment 8/assignement8.py
@@ -5,48 +5,45 @@
 ########################################################################
 #functions
 def dictionary_key_value():
-    with open("dictionary.txt",'a+') as file:#This stores the key:pair value of the sheng word and it's meaning
-        file.write(str(dictionary))
+    with open("dictionary.txt","a") as file:#This stores the key:pair value of the sheng word and it's meaning
+        file.write(json.dumps( dictionary))
 def dictionary_key():
-    with open("dictionary-key.txt", "a+") as file:# This file stores the key from the dictionary
-        file.write(str(dictionary.keys()))
+    with open("dictionary-key.txt", "a") as file:# This file stores the key from the dictionary
+        file.write(json.dumps(list(dictionary.keys())))
 def dictionary_value():
-    with open("dictionary-value.txt","a+") as file:# This file stores the values from the dictionary
-        file.write(str(dictionary.values()))
+    with open("dictionary-value.txt","a") as file:# This file stores the values from the dictionary
+        file.write(json.dumps(list(dictionary.values())))
 ########################################################################
-"""import json, requests
-data = 'https://raw.githubusercontent.com/normansimonr/Dumb-Cogs/master/lolz/tranzlashun.json'
+import json
+from pip._vendor import requests
+data = 'https://raw.githubusercontent.com/normansimonr/Dumb-Cogs/master/lolz/data/tranzlashun.json'
 resp = requests.get(data)
-print(resp)
-dictionary = eval(resp.text)
-#There seems to be a problem with the json fil
-print(dictionary)"""
+# print(resp)
+dictionary = json.loads(resp.text)
+with open("dictionary.txt","a") as file:
+    file.write(json.dumps(dictionary))#stores the dictionary in a text file
+# print(dictionary)
 
-
-dictionary = {}
-while True:
+while True:# a loop to ask the user to enter a new word or it's translation
     print("Do you want to add a new sheng word or get it's translation")
     print("Yes to continue: No to cancel")
-    answer = input().upper()
+    answer = input().upper()#converts the answer to capital letters
     if answer == "YES":
-            word = input('Original words: ')
-            if word in dictionary:
-                print('The translation is:', dictionary[word] )
-            elif word not in dictionary:
-                dictionary[word] =input("Enter it's meaning in English: ")
-            else:
-                
-                continue
+        word = input('Original words: ')
+        if word in dictionary:#if the word is in the dictionary it is printed
+            print('The translation is:', dictionary[word] )
+        elif word not in dictionary:#adds the word and it's meaning if it doesn't exist
+            dictionary[word] =input("Enter it's meaning in English: ")
+        else:
+            print("Enter a word")
     elif answer == "NO":
         print("Okay good luck")
         break
     else:
         print("Please choose yes or no")
-print(dictionary)
+# print(dictionary)
 print("This dictionary will be stored in a file")
+#the function calls adds the dictionaries in form of key and values separately and as key:values in one document
 dictionary_key_value()
 dictionary_key()
 dictionary_value()
-
-
-
